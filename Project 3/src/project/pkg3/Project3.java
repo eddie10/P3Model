2a11c9d5a0c94eee0eff86723b47d5d84ecce657
eddie10 <31421466+eddie10@users.noreply.github.com>
2018-11-28 18:00:59 -0500

    Add files via upload

---
+++ Project 3/src/project/pkg3/Project3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg3;
+
+/**
+ *
+ * @author Nikki
+ */
+public class Project3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Register view = new Register();
+        view.setVisible(true);
+    }
+    
+}
